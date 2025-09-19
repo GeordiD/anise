e0ee0c2169c4ecbc68fb307800e92f342fe7b34b
GeordiD <geordi.dosher@gmail.com>
2025-09-18 22:40:35 -0500

    y no work

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+console.log('enable swagger = ' + process.env.ENABLE_SWAGGER);
+
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
