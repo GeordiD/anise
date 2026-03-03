41f0ae78062f36479a9fc972aa17d42b31ac4c61
GeordiD <geordi.dosher@gmail.com>
2026-03-02 22:02:21 -0600

    dropdown background color

--- app/app.config.ts
+++ app/app.config.ts
@@ -19,6 +19,15 @@ export default defineAppConfig({
       slots: {
         base: 'placeholder:text-muted'
       },
+    },
+    dropdownMenu: {
+      compoundVariants: [{
+        color: 'neutral',
+        active: false,
+        class: {
+          item: 'data-highlighted:before:bg-accented/50 data-[state=open]:before:bg-accented/50'
+        }
+      }]
     }
   },
 });
